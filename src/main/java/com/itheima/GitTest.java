bb0ac60eb9b77b2b4cdfd07b211cb3e592071840
sh <sh@gitx.com>
2022-05-20 13:45:02 +0800

    push commit

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("hot-fix test222");
+        System.out.println("push test");
     }
 }
